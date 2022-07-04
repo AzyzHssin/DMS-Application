9a0aad10f21fedc67382fbd10c46cd1baa270c16
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-04 12:41:20 +0100

    connection datatbse

---
+++ server/database/config.js
@@ -0,0 +1,8 @@
+//mysql config file (put your root and password that you are using it in your computer)
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'azerty09997414SAMEH', 
+    database: 'Posts', 
+    insecureAuth: true,
+}
