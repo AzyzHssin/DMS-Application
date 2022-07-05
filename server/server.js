80be199ef897826ab51a32a7947c4d1eebdd0f93
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-05 17:54:45 +0100

    fixing requests

--- server/server.js
+++ server/server.js
@@ -13,14 +13,14 @@ const Port = 3001
 
 
 
-app.use('/getusers', require('./routes/users.js'));
+app.use('/users', require('./routes/users.js'));
 
 //To test in thunder you have to put the path of server.js+ path routes.js like this (http://localhost:3001/market/get)
 
 app.use('/market', require('./routes/market.js'));
 
 //To test in thunder you have to put the path of server.js + the path that exist in routes like this (http://localhost:3001/getproduct/getproduct)
-app.use('/getproduct',require('./routes/product.js'));
+app.use('/product',require('./routes/product.js'));
 
 app.listen(Port,() =>{
     console.log('Server is working on http://localhost:'+Port);
