86f6e48e52579fc045f77e8aacc2a2819a097462
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-06 10:02:12 +0100

    fixing front

--- client/src/Components/OneProduct.jsx
+++ client/src/Components/OneProduct.jsx
@@ -4,8 +4,8 @@ const OneProduct = (props) => (
     
     <div className="ProductContainer">
       <h2 className="Title">{props.info.title} for sale</h2>
-      <img className="image" src={props.info.image_url}/>
-      <p className="price">{props.info.price}$</p>
+      <img className="image" alt="img" src={props.info.image_url}/>
+      <p className="price">{props.info.price}</p>
       <p className="description">{props.info.description}</p>
        <p className="quantite">{props.info.quantite}</p> 
        <p className="tel">{props.info.tel}</p> 
