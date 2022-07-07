1a5249cd8d56410c230369e3d46259006d8f621f
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-07 21:09:19 +0100

    fixing signup component

--- client/src/Components/SellYourProduct.jsx
+++ client/src/Components/SellYourProduct.jsx
@@ -12,25 +12,25 @@ class SellYourProduct extends React.Component{
  <div>
       <form className="" onSubmit={""}>
                 <label className="text-form" >Title:
-                <input type="text" name="name" value={""} onChange={""} required /><br></br>
+                <input type="text" name="name" value={""} onChange={""} placeholder="Title" required /><br></br>
                 </label><br></br>
-                <label className="">Image:
-                <input type="text" name='image' value={""} onChange={""} required /><br></br>
+                <label className="text-form">Image:
+                <input type="text" name='image' value={""} onChange={""} placeholder="Image URL" required /><br></br>
                 </label><br></br>
                 <label  className="text-form">Price:
-                <input type="url" name="price" value={""} onChange={""}  required/><br></br>
+                <input type="url" name="price" value={""} onChange={""} placeholder="Price"  required/><br></br>
                 </label><br></br>
                 <label  className="text-form">Description:
-                <input type="url" name="description" value={""} onChange={""}  required/><br></br>
+                <input type="url" name="description" value={""} onChange={""} placeholder="Description"  required/><br></br>
                 </label><br></br>
                 <label  className="text-form">Categorie:
-                <input type="url" name="categorie" value={""} onChange={""}  required/><br></br>
+                <input type="url" name="categorie" value={""} onChange={""} placeholder="Categorie"  required/><br></br>
                 </label><br></br>
                 <label  className="text-form">Quantite:
-                <input type="url" name="quantite" value={""} onChange={""}  required/><br></br>
+                <input type="url" name="quantite" value={""} onChange={""} placeholder="Qunatite"  required/><br></br>
                 </label><br></br>
                 <label  className="text-form">Telephone:
-                <input type="url" name="telephone" value={""} onChange={""}  required/><br></br>
+                <input type="url" name="telephone" value={""} onChange={""} placeholder="Telephone"  required/><br></br>
                 </label><br></br>
                 <input type="submit" className="btn-submit" value='Submit'/> 
 
