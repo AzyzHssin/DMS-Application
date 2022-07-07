af158651c8b9cb4f0bf7636dda1bc26e5526ae9c
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-07 21:24:53 +0100

    t

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,12 +2,12 @@ const product = require('../database/models/product.js');
 
 module.exports = {
     getProduct: (req,res)=>{
-        console.log("testing")
+        console.log("Product table data")
         product.getAllProduct((err,result)=>{
             if(err) {
                 res.status(500).send()
             }else {
-                res.send(result)
+                res.json(result)
             }
         })
     }
