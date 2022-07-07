542ef54861753c241045f131cab96d0d88e1e6d5
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-07-07 21:30:24 +0100

    testing

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,7 +2,7 @@ const product = require('../database/models/product.js');
 
 module.exports = {
     getProduct: (req,res)=>{
-        console.log("Product table data")
+        console.log("This is product table")
         product.getAllProduct((err,result)=>{
             if(err) {
                 res.status(500).send()
